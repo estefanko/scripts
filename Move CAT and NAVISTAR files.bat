2a2444efd327bda9263a350fde92640e2d948fa7
Eric Stefanko <74471609+estefanko@users.noreply.github.com>
2021-01-29 14:49:20 -0500

    Update Move CAT and NAVISTAR files.bat
    Changed .txt file extension to .edi and added clarification to some comments

--- Move CAT and NAVISTAR files.bat
+++ Move CAT and NAVISTAR files.bat
@@ -1,15 +1,15 @@
-:: This script creates a folder with today's date in MM-DD-YY format and moves the CATPV and CATDX files into it. Then moves the newly created folder into the Perkins Not Processed Folder. This script must placed in the Z:\CatFTP\Inbox folder to work. ::
+:: This script creates a folder with today's date in MM-DD-YY format and moves the CATPV and CATDX EDI files into it. Then moves the newly created folder into the Perkins Not Processed Folder. This script must placed in the Z:\CatFTP\Inbox folder to work. ::
+:: Also moves the NAVISTAR files into the Navisator folder ::
 
 :: Create a new folder in MM-DD-YY format ::
 MKDIR %date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%
 
 :: Moves CATPV and CATDX files into newly created folder.::
-:: MAKE SURE TO CHANGE FILE EXTENSION IF NECESSARY::
-MOVE /-Y CATDX*.txt "%date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%"
-MOVE /-Y CATPV*.txt "%date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%"
+MOVE /-Y CATDX*.edi "%date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%"
+MOVE /-Y CATPV*.edi "%date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%"
 
-:: Moves newly created folder with CATPV and CATDX files into Perkins Not Processed ::
+:: Moves newly created folder with CATPV and CATDX files into Perkins Not Processed folder ::
 MOVE /-Y "%date:~-10,2%"-"%date:~7,2%"-"%date:~-2,2%" "Perkins Not Processed"
 
-:: Moves NAVISTAR Files into the NAVISTAR Folder ::
+:: Moves NAVISTAR files into the NAVISTAR folder ::
 MOVE /-Y "Z:\SoftshareFTP\Inbox\NAVISTAR-862-862-*.edi" "Z:\SoftshareFTP\Inbox\Navistar 862"
