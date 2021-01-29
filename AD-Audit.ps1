daedc3c7baff09dc2e2e3b13bc08e68c5ea9fd5b
Eric Stefanko <74471609+estefanko@users.noreply.github.com>
2021-01-29 15:12:16 -0500

    Update AD-Audit.ps1
    Changed the filepath to use the current userprofile as to be more variable

--- AD-Audit.ps1
+++ AD-Audit.ps1
@@ -1,10 +1,12 @@
-﻿#PowerShell script to output users and user properties from Active Directory into a CSV file
+#PowerShell script to output users and user properties from Active Directory into a CSV file
 
 Import-Module ActiveDirectory
 
+$UserProfile = $env:USERPROFILE
+
 $FileName = Read-Host "Enter in what you want the filename to be"
 
-Get-ADUser -Filter * -Properties DisplayName,SamAccountName,EmailAddress,LastLogonDate,Department,Title,Company | Export-Csv -Path C:\Users\[ENTER PATH USERNAME HERE]\Documents\$FileName.csv -NoTypeInformation
+Get-ADUser -Filter * -Properties DisplayName,SamAccountName,EmailAddress,LastLogonDate,Department,Title,Company | Export-Csv -Path $UserProfile\Documents\$FileName.csv -NoTypeInformation
 
 Write-Output "Your file has been saved to \Documents\$FileName.csv"
 
