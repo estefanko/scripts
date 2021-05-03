ad925a321e78713a1c0842507ebc655b5df01ac4
Eric Stefanko <74471609+estefanko@users.noreply.github.com>
2021-05-03 16:22:51 -0400

    Update DisableUser.ps1
    Added password generator and reset password sections.
    Added section of add security/distribution groups to the notes section of the telephone tab before removing them.
    Changed some wording.

--- DisableUser.ps1
+++ DisableUser.ps1
@@ -1,26 +1,47 @@
 Import-Module ActiveDirectory
 
-#Disable the user and move them to the Disabled users OU
+#Generate a random-ish password
+$MinimumPasswordLength = 30
+$MaximumPasswordLength = 50
+$PasswordLength = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+$MinimumNonAlphaNumericCharacters = 8
+$MaximumNonAlphaNumericCharacters = 16
+$RandomNonAlphaNumericCharacters = Get-Random -Minimum $MinimumNonAlphaNumericCharacters -Maximum $MaximumNonAlphaNumericCharacters
+Add-Type -AssemblyName System.Web
+$GeneratedPassword = [System.Web.Security.Membership]::GeneratePassword($PasswordLength, $RandomNonAlphaNumericCharacters)
+$SecurePassword = ConvertTo-SecureString -String $GeneratedPassword -AsPlainText -Force
+
+#Get the user information
 $UserAccount = Read-Host -Prompt "Enter the username of the account you'd like to disable"
 $UserGUID= Get-ADUser -Identity $UserAccount -Properties ObjectGUID
-$DisabledOUGUID = Get-ADOrganizationalUnit -Identity "OU=Users,OU=Disabled,DC=[REDACTED],DC=[REDACTED]" -Properties ObjectGUID #MAKE SURE TO CHANGE THE DOMAIN CONTROLLERS
 
+#Reset password for the user
+Set-ADAccountPassword -Identity $UserGUID -NewPassword $SecurePassword -Confirm:$False #-Confirm:$False disables the interactive prompt
+Write-Host "Reset the password for $UserAccount"
+
+#Disable User and move them to the disabled users OU
+$DisabledOUGUID = Get-ADOrganizationalUnit -Identity "OU=Users,OU=Disabled,DC=[REDACTED],DC=[REDACTED]" -Properties ObjectGUID #MAKE SURE TO CHANGE THE DOMAIN CONTROLLERS
 Disable-ADAccount -Identity $UserGUID
 Move-ADObject -Identity $UserGUID -TargetPath $DisabledOUGUID
 
-Write-Host "Successfully disabled AD Account $UserAccount"
-Write-Host "Successfully moved $UserAccount to Disabled\Users Organizational Unit"
+Write-Host "Disabled the AD Account for $UserAccount"
+Write-Host "Moved $UserAccount to Disabled\Users Organizational Unit"
 
 #Set account description to the time it was disabled
 $CurrentDate = Get-Date -Format g #"g" is .NET date formatting not specified in the PowerShell help files itself
 $UserDescription = "Disabled on $CurrentDate"
 Set-ADUser -Identity $UserAccount -Description $UserDescription
-Write-Host "Set user description to disabled date"
+Write-Host "Set $UserAccount description to the current time"
+
+#Get the security and distribution groups and add them to the Telephone Notes section of the user properties
+[string]$UserGroups = Get-ADPrincipalGroupMembership -Identity $UserAccount | Select-Object -ExpandProperty Name
+Get-ADUser -Identity $UserAccount -Properties info | Set-ADUser -Replace @{info = "Removed from security/distribution groups: `n`r`n`r$UserGroups"} #I don't know how to format this properly
+Write-Host "Added security/distribution groups to the Notes section of the Telephone tab in the AD user properties"
 
 #Remove user from security and distribution groups
-$UserMemberOf = Get-ADUser -Identity $UserAccount -Properties MemberOf
-$UserGroups = $UserMemberOf.MemberOf
-
-$UserGroups | Remove-ADGroupMember -Members $UserAccount -Confirm:$False #-Confirm:$False disables the interactive prompt asking to confirm if you want to remove users from a security/distribution group
+$UserMemberOF = Get-ADUser -Identity $UserAccount -Properties MemberOf
+$MemberOfGroups = $UserMemberOF.MemberOf
+$MemberOfGroups | Remove-ADGroupMember -Members $UserAccount -Confirm:$False
+Write-Host "Remove security/distriution group access from $UserAccount"
 
 Start-Sleep -Seconds 5
