91a40ca75bac09854d21cc36d3194db2fbcc15ac
Eric Stefanko <74471609+estefanko@users.noreply.github.com>
2021-06-13 17:11:21 -0400

    Delete Outlook Password Prompt Registry Fix.ps1

--- Outlook Password Prompt Registry Fix.ps1
+++
@@ -1,6 +0,0 @@
-#This creates a DWORD key in the registry that fixes the Outlook issue where it constantly asks for a password in the lower right toolbar.
-#Make sure the user has saved their work before running this script as it will restart their computer.
-
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\16.0\Common\Identity" -Name "DisableADALatopWAMOverride" -PropertyType "DWORD" -Value 1 -Force
-
-Restart-Computer
