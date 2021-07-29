d1963ca9ba8aaf550bb5b4e69c2ead7e0aa74a17
Eric Stefanko <74471609+estefanko@users.noreply.github.com>
2021-07-29 11:42:14 -0400

    Update AD Update Script [REDACTED].ps1
    Added more validation for managers. Added work location fields.

--- AD Update Script [REDACTED].ps1
+++ AD Update Script [REDACTED].ps1
@@ -1,11 +1,11 @@
 Import-Module ActiveDirectory
 
 #Import the most recent CSV file
-$CSVFile = Import-Csv (Get-ChildItem -Path "[REDACTED PATH]\*.csv" -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1)
+$CSVFile = Import-Csv (Get-ChildItem -Path "[REDACTED]\*.csv" -File | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1)
 
 #Remove the previous log file
-if ((Test-Path -Path "[REDACTED PATH].txt") -eq $True){
-    Remove-Item -Path "[REDACTED PATH].txt"
+if ((Test-Path -Path "[REDACTED].txt") -eq $True){
+    Remove-Item -Path "[REDACTED].txt"
 }
 
 #Start of Output
@@ -27,19 +27,35 @@ ForEach ($User in $CSVFile){
         $UserObject = Get-ADUser -Filter {(GivenName -like $FirstName) -and (Surname -like $LastName)}
         $Department = $User."Department Name"
         $Title = $User."Job Title"
+        $WorkLocation = $User."Work Location"
+
+        if ($WorkLocation -like [REDACTED]){
+            $StreetAddress = "[REDACTED]"
+        }
+        elseif ($WorkLocation -like "[REDACTED]") {
+            $StreetAddress = "[REDACTED]"
+        }
+        else {
+            $StreetAddress = "Remote"
+        }
         
         #Change user properties based on the the CSV data
-        Set-ADUser -Identity $UserObject -Manager $ManagerObject -Department $Department -Title $Title
+        if ($Null -eq $ManagerObject) { #ManagerObject -eq $null would be the case if either their legal first name is different than the one in Active Directory (e.g. Michael vs Mike). We don't update their manager in this case because otherwise it makes the field blank and messes up the org chart.
+            Set-ADUser -Identity $UserObject -Department $Department -Title $Title -StreetAddress $StreetAddress
+        }
+        else {
+            Set-ADUser -Identity $UserObject -Manager $ManagerObject -Department $Department -Title $Title -StreetAddress $StreetAddress
+        }
     }
 
     #If this error appears for a user with an AD account, check the for their first name in the CSV data vs their first name in AD. Often it will be a difference between their legal first name in the CSV and their nickname in AD (e.g. Michael vs Mike).
     catch [System.Management.Automation.ParameterBindingException]{
         Write-Host "$FirstName $LastName" -ForegroundColor Red -BackgroundColor Black -NoNewline
         Write-Host " was not found in Active Directory"
-        "$FirstName $LastName" | Out-File -FilePath "[REDACTED PATH].txt" -Append
+        "$FirstName $LastName" | Out-File -FilePath "[REDACTED].txt" -Append
     }
 }
 
-Write-Host "`r`n`r`nThe error log can be found at [REDACTED PATH].txt`r`n`r`n" -ForegroundColor Yellow
+Write-Host "`r`n`r`nThe error log can be found at [REDACTED].txt`r`n`r`n" -ForegroundColor Yellow
 
 Read-Host "Press enter to exit the program"
